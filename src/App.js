d0e132d8788ff310b1664ad6cb7393c76c2c69ed
Blen Redwan <blenredwan@gmail.com>
2024-10-02 11:29:09 +0300

    banner and row final touch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./compnents/header/Header.js";
 import Fotter from "./compnents/fotter/Fotter.js"
 import './App.css';
+import Banner from "./compnents/banner/Banner.js";
+import RowList from "./compnents/rows/rowlist/RowList.js";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <RowList />
       <Fotter />
       
     </div>
---
+++ src/compnents/banner/Banner.js
@@ -0,0 +1,51 @@
+
+import React, { useEffect, useState } from 'react'
+import requests from '../../utils/requests'
+import axios from '../../utils/axios';
+import './banner.css'
+
+const Banner = () => {
+  const [movie, setMovie]= useState({});
+  useEffect(() => {
+    (async() => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals)
+        setMovie(request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ])
+      }catch (error){
+        console.log("error", error);
+      }
+      
+    })()
+  }, [])
+
+  function truncate(str, n){
+    return str?.length > n ? str.substr(0, n-1)+ '...': str;
+  }
+  return (
+    <div className='banner' 
+    style={{
+      backgroundSize :"cover",
+      backgroundImage :`url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")` ,
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat"
+    }}
+      >
+        <div className='banner_contents'>
+          <h1 className='banner_title'>
+            {movie?.title || movie?.name || movie?.orginal_name}
+          </h1>
+          <div className='banner_bottons'>
+            <button className='banner_botton play'>Play</button>
+            <button className='banner_botton'>My List</button>
+          </div>
+          <h1 className='overview'>{truncate(movie?.overview, 150)}</h1>
+          <div className='banner_fadeBottom'/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Banner
